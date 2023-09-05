5b912db26854fb7f8845b53e87441488fa10352f
JaakkoKui <jaakko.kuivasniemi@hotmail.com>
2023-09-05 20:19:08 +0300

    Optimized v.2

--- FirstTask.c
+++ FirstTask.c
@@ -3,13 +3,11 @@
 
 #define months 12
 #define studentNumber 45001
+#define maxStudents 100
 
-float number = 1.0;
-float sum = 0.0;
-float sum2 = 0.0;
-float sum3 = 0.0;
 int count = 0;
 float salaries[months];
+float yearlyTotal = 0.0;
 int i;
 
 float average(float sum, int count);
@@ -24,6 +22,7 @@ float average(float sum, int count)
 
 float salaryCalc(int i)
 {
+    
     printf("Give the salary of month %d\n", i + 1);
     scanf("%f", &salaries[i]);
     if (salaries[i] < 0)
@@ -32,8 +31,8 @@ float salaryCalc(int i)
     }
     else
     {
-        sum2 += salaries[i];
-        printf("Sum: %.2f\n", sum2);
+        yearlyTotal += salaries[i];
+        printf("Sum: %.2f\n", yearlyTotal);
     }
 }
 
@@ -42,6 +41,7 @@ int main(void)
     // This is the main menu of the program, it asks the user to choose a task to run.
     while (1)
     {
+
         printf("Choose task to run:\n1. Average of given numbers\n2. Yearly salary calculator \n3. Student grader \n0. Exit\n");
         int task;
         if (scanf("%d", &task) != 1)
@@ -55,6 +55,8 @@ int main(void)
 
         // This calculates the average of numbers given by the user, no negative numbers allowed and 0 ends the feed.
         case 1:
+            float number = 1.0;
+            float sum = 0.0;
             do
             {
                 printf("Give numbers that you want the average calculated of one by one, no negative numbers and 0 ends the feed.\n");
@@ -88,18 +90,19 @@ int main(void)
             {
                 printf("Month [%2d]: %.2f\n", i + 1, salaries[i]);
             }
-            printf("Average of monthly salary: %.2f \n", average(sum2, 12));
-            printf("Yearly income: %.2f\n", sum2);
-            sum2 = 0;
+            printf("Average of monthly salary: %.2f \n", average(yearlyTotal, 12));
+            printf("Yearly income: %.2f\n", yearlyTotal);
+            yearlyTotal = 0;
             break;
 
         // This calculates the average of the grades of students given by the user, numbers between 1 - 5 allowed.
         case 3:
+            float gradesTotal = 0.0;
             int size;
-            printf("Give the number of students\n");
+            printf("Give the number of students, no negative or over 100\n");
             scanf("%d", &size);
 
-            if (size > 0)
+            if (size > 0 && size <= maxStudents)
             {
                 int array[size];
                 // intialize array with 0
@@ -117,7 +120,7 @@ int main(void)
                     if (grade > 0 && grade < 6)
                     {
                         array[i] = grade;
-                        sum3 += grade;
+                        gradesTotal += grade;
                     }
                     else
                     {
@@ -128,12 +131,12 @@ int main(void)
                 {
                     printf("Student %d: %d\n", i + studentNumber, array[i]);
                 }
-                printf("Average of student grades: %.2f \n", average(sum3, size));
-                sum3 = 0;
+                printf("Average of student grades: %.1f \n", average(gradesTotal, size));
+                gradesTotal = 0;
             }
             else
             {
-                printf("No negative numbers\n");
+                printf("No negative numbers or number over 100.\n");
             }
             break;
 
