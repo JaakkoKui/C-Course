00dd56be931af10eb69cf074741f09a99ae871c1
Jaakko Kuivasniemi <jaakkkui@metropolia.fi>
2023-09-11 20:17:55 +0300

    Task 4 v.2

--- SecondTask.c
+++ SecondTask.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
+void ifIsInvalid() {
+    printf("Invalid input!\n");
+    while (getchar() != '\n');
+}
 int main(void) {
     while (1) {
         printf("Choose task to run:\n4.Calulate scalar product of two given 3 number vectors. \n5.\n6.\n0. Exit\n");
-
         int task;
         if (scanf("%d", &task) != 1) {
-            printf("Invalid input!\n");
-            while (getchar() != '\n');
+            ifIsInvalid();
         } else {
             switch (task) {
                 case 4: {
                     printf("Enter numbers for vector 1:\n");
-                    int n1, n2, n3;
-                    if (scanf("%d %d %d", &n1, &n2, &n3) != 3) {
-                        printf("Invalid input!\n");
-                        while (getchar() != '\n');
+                    float n1, n2, n3;
+                    if (scanf("%f %f %f", &n1, &n2, &n3) != 3) {
+                        ifIsInvalid();
                     } else {
                         printf("Enter numbers for vector 2:\n");
-                        int m1, m2, m3;
-                        if (scanf("%d %d %d", &m1, &m2, &m3) != 3) {
-                            printf("Invalid input!\n");
-                            while (getchar() != '\n');
+                        float m1, m2, m3;
+                        if (scanf("%f %f %f", &m1, &m2, &m3) != 3) {
+                            ifIsInvalid();
                         } else {
-                            printf("Numbers of vectors:\nVector 1: %d %d %d\nVector 2: %d %d %d\n", n1, n2, n3, m1, m2, m3);
-                            int scalar = n1 * m1 + n2 * m2 + n3 * m3;
-                            printf("Scalar product of vectors is %d\n\n", scalar);
+                            printf("Numbers of vectors:\nVector 1: %g %g %g\nVector 2: %g %g %g\n", n1, n2, n3, m1, m2, m3);
+                            float scalar = n1 * m1 + n2 * m2 + n3 * m3;
+                            printf("Scalar product of vectors is %g \n\n", scalar);
                         }
                     }
                     break;
