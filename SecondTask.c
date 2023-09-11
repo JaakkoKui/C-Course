f1cbbf79847b7578d83fc06e6578128e85fbbb5f
Jaakko Kuivasniemi <jaakkkui@metropolia.fi>
2023-09-11 19:37:17 +0300

    Task 4 done

--- SecondTask.c
+++ SecondTask.c
@@ -3,7 +3,7 @@
 
 int main(void) {
     while (1) {
-        printf("Choose task to run:\n1.\n2.\n3.\n0. Exit\n");
+        printf("Choose task to run:\n4.Calulate scalar product of two given 3 number vectors. \n5.\n6.\n0. Exit\n");
 
         int task;
         if (scanf("%d", &task) != 1) {
@@ -11,15 +11,32 @@ int main(void) {
             while (getchar() != '\n');
         } else {
             switch (task) {
-                case 1: {
+                case 4: {
+                    printf("Enter numbers for vector 1:\n");
+                    int n1, n2, n3;
+                    if (scanf("%d %d %d", &n1, &n2, &n3) != 3) {
+                        printf("Invalid input!\n");
+                        while (getchar() != '\n');
+                    } else {
+                        printf("Enter numbers for vector 2:\n");
+                        int m1, m2, m3;
+                        if (scanf("%d %d %d", &m1, &m2, &m3) != 3) {
+                            printf("Invalid input!\n");
+                            while (getchar() != '\n');
+                        } else {
+                            printf("Numbers of vectors:\nVector 1: %d %d %d\nVector 2: %d %d %d\n", n1, n2, n3, m1, m2, m3);
+                            int scalar = n1 * m1 + n2 * m2 + n3 * m3;
+                            printf("Scalar product of vectors is %d\n\n", scalar);
+                        }
+                    }
                     break;
                 }
                 
-                case 2: {
+                case 5: {
                     break;
                 }
 
-                case 3: {
+                case 6: {
                     break;
                 }
 
