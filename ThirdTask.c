e5dad9a0b512432453ee6d959203a1411243913b
JaakkoKui <jaakko.kuivasniemi@hotmail.com>
2023-09-20 11:42:28 +0300

    3.2 Started

--- ThirdTask.c
+++ ThirdTask.c
@@ -15,6 +15,15 @@ struct keyValue
     char value;
 } map[26];
 
+struct Student
+{
+    char name[20];
+    int studentNumber;
+    char streetAdress[40];
+    char city[20];
+    float totalCredits;
+};
+
 char *cipher(char *text, char *cipheredText, struct keyValue *map)
 {
     for (int i = 0; i < strlen(text); i++)
@@ -45,7 +54,7 @@ int main(void)
 {
     while (1)
     {
-        printf("Choose task to run:\n7.Cipher\n8.\n6.\n0. Exit\n");
+        printf("Choose task to run:\n7. Cipher\n8. Student information\n6. \n0. Exit\n");
         int task;
         if (scanf("%d", &task) != 1)
         {
@@ -57,8 +66,8 @@ int main(void)
             {
             case 7:
             {
-                const char text[100] = "";
-                const char cipheredText[100] = "";
+                char text[100] = "";
+                char cipheredText[100] = "";
                 const char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
                 const char reversedAlphabet[] = "zyxwvutsrqponmlkjihgfedcba";
 
@@ -70,7 +79,8 @@ int main(void)
                 }
 
                 printf("Enter text to cipher: ");
-                while (getchar() != '\n');
+                while (getchar() != '\n')
+                    ;
                 fgets(text, sizeof(text), stdin);
                 printf("\n");
                 cipher(text, cipheredText, map);
@@ -82,6 +92,28 @@ int main(void)
 
             case 8:
             {
+                struct Student students[15] = {
+                    {"Santeri", 666, "Nigeriapolku 90", "Nigeria", 0},
+                    {"Jaska", 432, "Xhingxhong 22", "Kiina", 44},
+                    {"Pekka", 672, "Keskikatu 2", "Suomi", 23},
+                    {"Matti", 999, "Pingpong 4", "Thaimaa", 55},
+                    {"Teppo", 111, "Gyros 5", "Kreikka", 12},
+                    {"Jorma", 212, "Gyros 6", "Kreikka", 41},
+                    {"Joonas", 515, "Zhangping 7", "Kiina", 22},
+                    {"Jussi", 231, "Bangkok 852352", "Thaimaa", 99},
+                    {"Jani", 777, "Mutaoja 77 B4", "Suomi", 32},
+                    {"Mikko", 123, "Sakura 11", "Japani", 75},
+                    {"Anna", 888, "Esplanadi 13 G66", "Suomi", 65},
+                    {"Laura", 345, "Colosseum Ave.", "Italy", 50},
+                    {"Hans", 567, "Brandenburg St.", "Saksa", 47},
+                    {"Carlos", 456, "Siesta Lane", "Kreikka", 33},
+                    {"Elena", 111, "Kremlin St.", "Venäjä", 28},
+                };
+
+                for (int i = 0; i < sizeof(students) / sizeof(students[0]); i++)
+                {
+                    printf("Name: %s\nStudent number: %d\nStreet adress: %s\nCity: %s\nTotal credits: %.1f\n\n", students[i].name, students[i].studentNumber, students[i].streetAdress, students[i].city, students[i].totalCredits);
+                }
                 break;
             }
 
