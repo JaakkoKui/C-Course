8ac0b71f82f53f5f6269ceaf5730656bbeef545a
JaakkoKui <jaakko.kuivasniemi@hotmail.com>
2023-09-26 13:48:35 +0300

    Task 3 2/3 done

--- ThirdTask.c
+++ ThirdTask.c
@@ -23,7 +23,7 @@ struct Student
     char city[20];
     float totalCredits;
 };
-
+int amount = 0;
 char *cipher(char *text, char *cipheredText, struct keyValue *map)
 {
     for (int i = 0; i < strlen(text); i++)
@@ -50,11 +50,25 @@ char *cipher(char *text, char *cipheredText, struct keyValue *map)
     return 0;
 }
 
+int findInCity(struct Student *students, int size, char *city)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        if (stricmp(students[i].city, city) == 0)
+        {
+            printf("%-15s %5d %-20s %-10s %5.1f\n", students[i].name, students[i].studentNumber, students[i].streetAdress, students[i].city, students[i].totalCredits);
+            amount++;
+        }
+    }
+    return amount;
+}
+
 int main(void)
 {
     while (1)
     {
-        printf("Choose task to run:\n7. Cipher\n8. Student information\n6. \n0. Exit\n");
+        printf("Choose task to run:\n7. Cipher\n8. Student information\n9. Item prices\n0. Exit\n");
         int task;
         if (scanf("%d", &task) != 1)
         {
@@ -92,29 +106,53 @@ int main(void)
 
             case 8:
             {
-                struct Student students[15] = {
-                    {"Santeri", 666, "Nigeriapolku 90", "Nigeria", 0},
-                    {"Jaska", 432, "Xhingxhong 22", "Kiina", 44},
-                    {"Pekka", 672, "Keskikatu 2", "Suomi", 23},
-                    {"Matti", 999, "Pingpong 4", "Thaimaa", 55},
-                    {"Teppo", 111, "Gyros 5", "Kreikka", 12},
-                    {"Jorma", 212, "Gyros 6", "Kreikka", 41},
-                    {"Joonas", 515, "Zhangping 7", "Kiina", 22},
-                    {"Jussi", 231, "Bangkok 852352", "Thaimaa", 99},
-                    {"Jani", 777, "Mutaoja 77 B4", "Suomi", 32},
-                    {"Mikko", 123, "Sakura 11", "Japani", 75},
-                    {"Anna", 888, "Esplanadi 13 G66", "Suomi", 65},
-                    {"Laura", 345, "Colosseum Ave.", "Italy", 50},
-                    {"Hans", 567, "Brandenburg St.", "Saksa", 47},
-                    {"Carlos", 456, "Siesta Lane", "Kreikka", 33},
-                    {"Elena", 111, "Kremlin St.", "Venäjä", 28},
-                };
 
-                for (int i = 0; i < sizeof(students) / sizeof(students[0]); i++)
+                char findCity[20];
+                struct Student students[15] = {
+                    {"Santeri", 666, "Mannerheimintie 1", "Helsinki", 0},
+                    {"Jaska", 432, "Esplanadi 2", "Espoo", 44},
+                    {"Pekka", 672, "Aleksanterinkatu 3", "Tampere", 23},
+                    {"Matti", 999, "Rautatienkatu 4", "Turku", 55},
+                    {"Teppo", 111, "Kauppakatu 5", "Oulu", 12},
+                    {"Jorma", 212, "Kirkkokatu 6", "Helsinki", 41},
+                    {"Joonas", 515, "Hameenkatu 7", "Lahti", 22},
+                    {"Jussi", 231, "Hameenkatu 8", "Kuopio", 99.5},
+                    {"Jani", 777, "Helsinginkatu 9", "Lahti", 32},
+                    {"Mikko", 123, "Kauppakatu 10", "Oulu", 75},
+                    {"Anna", 888, "Keskuskatu 11", "Helsinki", 65},
+                    {"Laura", 345, "Kauppatori 12", "Turku", 50},
+                    {"Hans", 567, "Helsinginkatu 13", "Tampere", 47},
+                    {"Carlos", 456, "Kirkkokatu 14", "Tampere", 33},
+                    {"", 0, "", "", 0}};
+                int found = 0;
+                int sizeOfS = sizeof(students) / sizeof(students[0]);
+
+                for (int i = 0; i < sizeOfS; i++)
                 {
-                    printf("Name: %s\nStudent number: %d\nStreet adress: %s\nCity: %s\nTotal credits: %.1f\n\n", students[i].name, students[i].studentNumber, students[i].streetAdress, students[i].city, students[i].totalCredits);
+                    if (students[i].studentNumber != 0)
+                    {
+                        printf("Name: %s\nStudent number: %d\nStreet adress: %s\nCity: %s\nTotal credits: %.1f\n\n", students[i].name, students[i].studentNumber, students[i].streetAdress, students[i].city, students[i].totalCredits);
+                    }
                 }
-                break;
+
+                while (1)
+                {
+                    printf("Enter city to search: \n\n");
+                    scanf("%s", findCity);
+                    if (stricmp(findCity, "stop") == 0)
+                    {
+                        break;
+                    }
+                    found = findInCity(students, sizeOfS, findCity);
+                    if (found == 0)
+                    {
+                        printf("No students live in given city.\n\n");
+                    }
+                    else
+                    {
+                        printf("Students living in given city found: %d\n\n", found);
+                    }
+                }
             }
 
             case 9:
